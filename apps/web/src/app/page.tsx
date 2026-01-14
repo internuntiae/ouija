b5bb29c3c110a1bd9ed6f7aafd01d5162a6b36ea
Marek Zjeżdżałka <UnderscoreF17@gmail.com>
2026-01-14 21:39:39 +0100

    tiny changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,16 +5,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1>ouija</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{' '}
             <a
