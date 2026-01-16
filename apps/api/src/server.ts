c470c756fc295dba2bc59d6600bd42c1c25f850b
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2026-01-16 21:45:38 +0100

    configuration ts ( type shi )

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,11 +1,11 @@
 import express, { Express } from 'express'
-
-import { client } from './db/redis'
-
-async function main(){
-  await client.connect()
-}
+import { testRouter } from '@routers/router';
 const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api", testRouter);
 
 app.get('/', (req, res) => {
   res.status(200).json({ "ok": "okey"})
