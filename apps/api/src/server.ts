fb5efb814a86d2381daee4b2e02ef425c4664f96
Gustaw Grześkowiak <gustaw.grzeskowiak@gmail.com>
2026-01-19 22:28:12 +0100

    Logs

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,5 @@
 import express, { Express } from 'express'
 import testRouter from '@routers/router'
-import postgresClient from '@services/pg'
-import redisClient from '@services/redis'
 import health from '@services/db'
 
 const app: Express = express()
@@ -9,17 +7,6 @@ const app: Express = express()
 app.use('/', testRouter)
 
 app.use('/api', health)
-
-app.get('/', async (req, res) => {
-  const query = await postgresClient.query('SELECT * FROM test_table')
-  const name = await redisClient.get('user:1:name')
-  console.log(query.rows[0])
-  console.log(name)
-  res.json({
-    pg: query.rows[0],
-    user: name
-  })
-})
 
 app.listen(3001, () => {
   console.log('App is running on http://localhost:3001')
