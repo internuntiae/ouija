df84ed59cde774dc1d31134780ffdb4bf5552fcc
FMaciejewski <Filip2007_m@wp.pl>
2026-01-26 22:04:15 +0100

    chats page look

--- apps/web/src/app/chats/page.tsx
+++ apps/web/src/app/chats/page.tsx
@@ -4,83 +4,94 @@ import Image from 'next/image'
 export default function Chats() {
   return (
     <>
-      <div className={styles.Contacts}>
-        <div className={styles.ContactsChatPreview}>
-          <Image
-            className={styles.ContactsChatPreviewProfilePicture}
-            src={'/ouija_white.svg'}
-            alt={'profile_picture'}
-            height={30}
-            width={30}
-          ></Image>
-          <div className={styles.ContactsChatPreviewMessageContainer}>
-            <h4 className={styles.ContactsChatPreviewMessageContainerName}>
-              Jan Kowalski
-            </h4>
-            <p className={styles.ContactsChatPreviewMessageContainerMessage}>
-              Ty: Ej stary pomóż w projekcie
-            </p>
+      <div className={styles.container}>
+        <div className={styles.Contacts}>
+          <div className={styles.ContactsChatPreview}>
+            <Image
+              className={styles.ContactsChatPreviewProfilePicture}
+              src={'/ouija_white.svg'}
+              alt={'profile_picture'}
+              height={30}
+              width={30}
+            ></Image>
+            <div className={styles.ContactsChatPreviewMessageContainer}>
+              <h4 className={styles.ContactsChatPreviewMessageContainerName}>
+                Jan Kowalski
+              </h4>
+              <p className={styles.ContactsChatPreviewMessageContainerMessage}>
+                Ty: Ej stary pomóż w projekcie
+              </p>
+            </div>
+          </div>
+          <div className={styles.ContactsChatPreview}>
+            <Image
+              className={styles.ContactsChatPreviewProfilePicture}
+              src={'/ouija_white.svg'}
+              alt={'profile_picture'}
+              height={30}
+              width={30}
+            ></Image>
+            <div className={styles.ContactsChatPreviewMessageContainer}>
+              <h4 className={styles.ContactsChatPreviewMessageContainerName}>
+                Jan Kowalski
+              </h4>
+              <p className={styles.ContactsChatPreviewMessageContainerMessage}>
+                Ty: Ej stary pomóż w projekcie
+              </p>
+            </div>
+          </div>
+          <div className={styles.ContactsChatPreview}>
+            <Image
+              className={styles.ContactsChatPreviewProfilePicture}
+              src={'/ouija_white.svg'}
+              alt={'profile_picture'}
+              height={30}
+              width={30}
+            ></Image>
+            <div className={styles.ContactsChatPreviewMessageContainer}>
+              <h4 className={styles.ContactsChatPreviewMessageContainerName}>
+                Jan Kowalski
+              </h4>
+              <p className={styles.ContactsChatPreviewMessageContainerMessage}>
+                Ty: Ej stary pomóż w projekcie
+              </p>
+            </div>
           </div>
         </div>
-        <div className={styles.ContactsChatPreview}>
-          <Image
-            className={styles.ContactsChatPreviewProfilePicture}
-            src={'/ouija_white.svg'}
-            alt={'profile_picture'}
-            height={30}
-            width={30}
-          ></Image>
-          <div className={styles.ContactsChatPreviewMessageContainer}>
-            <h4 className={styles.ContactsChatPreviewMessageContainerName}>
-              Jan Kowalski
-            </h4>
-            <p className={styles.ContactsChatPreviewMessageContainerMessage}>
-              Ty: Ej stary pomóż w projekcie
-            </p>
+
+        <div className={styles.Chat}>
+          <div className={styles.ChatContactInfo}>
+            <h2>Jan Kowalski</h2>
+            <h5>Aktywny</h5>
           </div>
-        </div>
-        <div className={styles.ContactsChatPreview}>
-          <Image
-            className={styles.ContactsChatPreviewProfilePicture}
-            src={'/ouija_white.svg'}
-            alt={'profile_picture'}
-            height={30}
-            width={30}
-          ></Image>
-          <div className={styles.ContactsChatPreviewMessageContainer}>
-            <h4 className={styles.ContactsChatPreviewMessageContainerName}>
-              Jan Kowalski
-            </h4>
-            <p className={styles.ContactsChatPreviewMessageContainerMessage}>
-              Ty: Ej stary pomóż w projekcie
-            </p>
+          <div className={styles.ChatMessageContainer}>
+            <div className={styles.ChatMessageContainerYourMessage}>
+              <p>No co tam mordko </p>
+            </div>
+            <div className={styles.ChatMessageContainerContactMessage}>
+              <p>A dobrze </p>
+            </div>
+            <div className={styles.ChatMessageContainerYourMessage}>
+              <p>A to spoko </p>
+            </div>
+            <div className={styles.ChatMessageContainerYourMessage}>
+              <p>Ale możesz pomóc z projektem </p>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className={styles.Chat}>
-        <div className={styles.ChatContactInfo}>
-          <h2>Jan Kowalski</h2>
-          <h5>Aktywny</h5>
-        </div>
-        <div className={styles.ChatMessageContainer}>
-          <div className={styles.ChatMessageContainerYourMessage}>
-            <p>No co tam mordko </p>
+          <div className={styles.ChatMessageToolbar}>
+            <form>
+              <input
+                type={'text'}
+                placeholder={'wpisz wiadomość'}
+                className={styles.ChatMessageToolbarInput}
+              />
+              <input
+                type={'submit'}
+                value={'wyślij'}
+                className={styles.ChatMessageToolbarSubmit}
+              />
+            </form>
           </div>
-          <div className={styles.ChatMessageContainerYourMessage}>
-            <p>A dobrze </p>
-          </div>
-          <div className={styles.ChatMessageContainerYourMessage}>
-            <p>A to spoko </p>
-          </div>
-          <div className={styles.ChatMessageContainerYourMessage}>
-            <p>Ale możesz pomóc z projektem </p>
-          </div>
-        </div>
-        <div className={styles.ChatMessageToolbar}>
-          <form>
-            <input type={'text'} placeholder={'wpisz wiadomość'} />
-            <input type={'submit'} value={'wyślij'} />
-          </form>
         </div>
       </div>
     </>
